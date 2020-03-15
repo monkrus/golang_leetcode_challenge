c42407f86b5f4c18f4d071cf9d139de4f8b43a73
Sergei Stadnik <sergeisqa@gmail.com>
2020-03-15 13:52:50 -0700

    Update main.go

--- main.go
+++ main.go
@@ -93,4 +93,10 @@ func reverse(s []int) {
 	}   
 }
 
+4. Contains duplicate
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array,
+and it should return false if every element is distinct.
 
